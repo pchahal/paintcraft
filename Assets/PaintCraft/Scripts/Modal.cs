31a8d6560b4d4ecc0bc1e85fd9b9f1d027bb9b25
pchahal <pchahals@gmail.com>
2017-02-01 10:11:15 -0800

    update

--- Assets/PaintCraft/Scripts/Modal.cs
+++ Assets/PaintCraft/Scripts/Modal.cs
@@ -6,9 +6,7 @@ public class Modal : MonoBehaviour
 
     void ToggleButtons(bool flag)
     {
-        Painter painter = Camera.main.GetComponent<Painter>();
-        if (painter != null)
-            Camera.main.GetComponent<Painter>().enabled = flag;
+
         GameObject[] buttons = GameObject.FindGameObjectsWithTag("Button");
         foreach (var button in buttons)
         {
