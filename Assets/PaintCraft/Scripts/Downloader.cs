8a22cff42b067c2211f64c2035b25270e3649d32
Pardeep Chahal <par@MacBook-Pro.local>
2017-02-27 13:00:46 -0800

    update

---
+++ Assets/PaintCraft/Scripts/Downloader.cs
@@ -0,0 +1,28 @@
+﻿// Add this script to a GameObject. The Start() function fetches an
+// image from the documentation site.  It is then applied as the
+// texture on the GameObject.
+
+using UnityEngine;
+using System.Collections;
+
+public class Downloader : MonoBehaviour
+{
+
+
+
+    IEnumerator Start()
+    {
+        string[] textures = FileManager.Instance.GetAllFiles(Application.persistentDataPath, "*.png");
+
+
+        string url;
+        url = "file://" + Application.persistentDataPath + "/skin.png";
+
+        Texture2D tex;
+        tex = new Texture2D(8, 8, TextureFormat.DXT1, false);
+        WWW www = new WWW(url);
+        yield return www;
+        www.LoadImageIntoTexture(tex);
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+}
---
+++ Assets/PaintCraft/Scripts/Test.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+
+    Color[] defaultColors;
+    string filePath;
+
+    void Start()
+    {
+        filePath = Application.persistentDataPath + "/skin.png";
+
+
+    }
+
+    public void OnButtonSave()
+    {
+
+        defaultColors = new Color[64 * 64];
+        for (int i = 0; i < defaultColors.Length; i++)
+        {
+            if (i % 2 == 0)
+                defaultColors[i] = Color.red;
+            else
+                defaultColors[i] = Color.grey;
+        }
+
+        Texture2D tex = new Texture2D(64, 64);
+        tex.filterMode = FilterMode.Point;
+        tex.wrapMode = TextureWrapMode.Clamp;
+        tex.SetPixels(defaultColors);
+        tex.Apply();
+
+
+        FileManager.Instance.SaveTextureToFile(tex, filePath);
+
+    }
+
+
+
+
+    public void OnButtonLoad()
+    {
+
+        Sprite sprite = FileManager.Instance.GetSpriteFromPNG(filePath, new Rect(8, 48, 8, 8));
+        GetComponent<Image>().sprite = sprite;
+    }
+
+    public void OnButtonLoadAllFiles()
+    {
+        string[] textures = FileManager.Instance.GetAllFiles(Application.persistentDataPath, "*.png");
+        Sprite sprite = FileManager.Instance.GetSpriteFromPNG(textures[0], new Rect(8, 48, 8, 8));
+        GetComponent<Image>().sprite = sprite;
+    }
+
+    public void OnButtonSkins()
+    {
+        SceneManager.LoadScene("skins");
+
+    }
+}
